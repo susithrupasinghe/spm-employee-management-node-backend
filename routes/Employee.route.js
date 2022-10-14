80b0f36478beaa24f2719f999d3dc8c64aa1f9e0
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-10-14 19:29:06 +0530

    Employee cruds

--- routes/Employee.route.js
+++ routes/Employee.route.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { getAllEmployeesList } = require("../controllers/Employee.controller");
+const {
+     getAllEmployeesList,
+     registerEmployee,
+     updateEmployeeProfile,
+     deleteEmployee,
+    } = require("../controllers/Employee.controller");
 const router = express.Router();
 
 router.get("/all",getAllEmployeesList);
+router.post("/register", registerEmployee);
+router.delete("/:id", deleteEmployee);
+router.put("/updateprofile/:id", updateEmployeeProfile);
 
 module.exports = router;
