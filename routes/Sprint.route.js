d02279e123a059a93997b16d8d12afefcebbed3f
Susith Rupasinghe <susith03program@gmail.com>
2022-10-15 13:28:41 +0530

    sprint router added

--- routes/Sprint.route.js
+++ routes/Sprint.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 
 const {
-    addIssueToSprint , addFeedbackSprint,changeIssueStatusTodoToInProgress,changeIssueStatusInProgressToDone,closeSprintById
+    addIssueToSprint , addFeedbackSprint,changeIssueStatusTodoToInProgress,changeIssueStatusInProgressToDone,closeSprintById, getSprintIssues
 } = require("../controllers/Sprint.controller");
 
 closeSprintById
@@ -12,5 +12,6 @@ router.put("/:id/inprogress",changeIssueStatusTodoToInProgress);
 router.put("/:id/done",changeIssueStatusInProgressToDone);
 router.put("/addFeedback/:id",addFeedbackSprint);
 router.put("/close/:id",closeSprintById);
+router.get("/issues/:id", getSprintIssues);
 
 module.exports = router;
