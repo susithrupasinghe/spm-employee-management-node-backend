0d393d8bbfee3e649bc7b9cf9447a0951746432a
Susith Rupasinghe <susith03program@gmail.com>
2022-10-15 14:50:53 +0530

    Issue routes modified

--- routes/Issue.route.js
+++ routes/Issue.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const {
-    gellAllIssueToDo} = require("../controllers/Issue.controller");
+const { gellAllIssueToDo , updateIssue, getIssue} = require("../controllers/Issue.controller");
 const router = express.Router();
 
 
 router.get("/issueTodo/:id", gellAllIssueToDo);
+
+router.get("/:id", getIssue);
+
+router.patch("/:id", updateIssue);
 
 module.exports = router;
