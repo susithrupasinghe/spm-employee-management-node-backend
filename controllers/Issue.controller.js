c9f3e395ccf8a7d33ef61fe93a56ca40e8c3491e
Susith Rupasinghe <susith03program@gmail.com>
2022-10-15 13:29:24 +0530

    Issue controller added

--- controllers/Issue.controller.js
+++ controllers/Issue.controller.js
@@ -1,4 +1,40 @@
 const express = require("express");
+const EmployeeModel = require("../models/Employee.model");
+const IssueModel = require("../models/Issue.model");
 const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+    try{
+        let id = req.params.id;
+        let issue = await IssueModel.findById(id);
+        //console.log(issue);
+        res.json(issue);
+    }catch(e){
+        res.sendStatus(500);
+        console.error(e);
+    }
+});
+
+router.patch("/:id", async (req, res) => {
+    try {
+        let id = req.params.id;
+        let { issueName, description, points, assignee, progress, estimatedTime } = req.body;
+        let update = {
+            issueName,
+            description,
+            points,
+            assignee,
+            progress,
+            estimatedTime
+        };
+    
+        console.log(id, update);
+        await IssueModel.findByIdAndUpdate(id, update);
+        res.sendStatus(200);
+    }catch(e) {
+        res.sendStatus(500);
+        console.error(e);
+    }
+});
 
 module.exports = router;
